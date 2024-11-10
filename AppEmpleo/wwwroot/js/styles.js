c41cfeb7f8a4cb21623855c501961f4ce9b68469
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-10 00:57:41 -0500

    Add styles for register page

---
+++ AppEmpleo/wwwroot/js/styles.js
@@ -0,0 +1,11 @@
+﻿document.querySelectorAll("input").forEach(input => {
+    // Agrega la clase "activo" cuando el input recibe el foco
+    input.addEventListener("focus", () => {
+        input.classList.add("register__input--active");
+    });
+
+    // Quita la clase "activo" cuando el input pierde el foco
+    input.addEventListener("blur", () => {
+        input.classList.remove("register__input--active");
+    });
+});
