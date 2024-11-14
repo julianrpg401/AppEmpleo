02fa7f4090f95be2a5cb848a9cb7a29439386233
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-14 17:39:45 -0500

    Add data annotations

--- AppEmpleo/Models/Usuario.cs
+++ AppEmpleo/Models/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppEmpleo.Models;
 
@@ -12,8 +13,11 @@ public partial class Usuario
 
     public string Apellido { get; set; } = null!;
 
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
     [DisplayName("Contraseña")]
     public string ClaveHash { get; set; } = null!;
 
