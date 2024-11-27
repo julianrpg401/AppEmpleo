2013fea04605d8a4070914e3f8438ffd18fc7695
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-26 23:07:13 -0500

    Add repository pattern

---
+++ AppEmpleo/Class/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using AppEmpleo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppEmpleo.Class
+{
+    public class UserRepository
+    {
+        private readonly AppEmpleoContext _appEmpleoContext;
+
+        public UserRepository(AppEmpleoContext appEmpleoContext)
+        {
+            _appEmpleoContext = appEmpleoContext;
+        }
+
+        // Validar si el correo electrónico ya está registrado
+        public async Task<Usuario?> ValidateExistingUserAsync(Usuario usuario)
+        {
+            var existingUser = await _appEmpleoContext.Usuarios.FirstOrDefaultAsync
+                (u => u.Email == usuario.Email);
+
+            return existingUser;
+        }
+
+        public async Task AddUserAsync(Usuario usuario)
+        {
+            await _appEmpleoContext.Usuarios.AddAsync(usuario);
+            await _appEmpleoContext.SaveChangesAsync();
+        }
+    }
+}
