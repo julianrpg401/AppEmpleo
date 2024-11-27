afebfc2b138497d288153dfbe343158561c51649
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-11-26 22:47:43 -0500

    Add validations

--- AppEmpleo/Models/Usuario.cs
+++ AppEmpleo/Models/Usuario.cs
@@ -9,15 +9,16 @@ public partial class Usuario
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El campo no puede estár vacío")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo no puede estár vacío")]
     public string Apellido { get; set; } = null!;
 
-    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
     [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
-    [Required(ErrorMessage = "La contraseña es obligatoria.")]
+    [Required(ErrorMessage = "El campo no puede estár vacío")]
     [DisplayName("Contraseña")]
     public string ClaveHash { get; set; } = null!;
 
@@ -25,6 +26,7 @@ public partial class Usuario
 
     public DateTime FechaRegistro { get; set; }
 
+    [Required(ErrorMessage = "El campo no puede estár vacío")]
     [DisplayName("Fecha de nacimiento")]
     public DateOnly FechaNacimiento { get; set; }
 
