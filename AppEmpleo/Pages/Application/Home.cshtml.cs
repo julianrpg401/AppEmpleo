b0b457db02cc4164c0c254902d31ab1bec19c0ed
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-01 23:27:52 -0500

    Offer repository inject

--- AppEmpleo/Pages/Application/Home.cshtml.cs
+++ AppEmpleo/Pages/Application/Home.cshtml.cs
@@ -1,12 +1,27 @@
+using AppEmpleo.Class;
+using AppEmpleo.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AppEmpleo.Pages.LandingPage
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        private readonly OfferRepository _offerRepository;
+
+        [BindProperty]
+        public List<Oferta> Ofertas { get; set; } = new List<Oferta>();
+
+        public HomeModel(OfferRepository offerRepository)
         {
+            _offerRepository = offerRepository;
+        }
+
+        public async void OnGetAsync()
+        {
+            Ofertas = await _offerRepository.GetListAsync(Ofertas);
         }
 
         public async Task<IActionResult> OnPostAsync()
