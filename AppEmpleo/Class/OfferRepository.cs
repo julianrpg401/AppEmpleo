099b0e52bd108e5182746ea595fae9a00168fc05
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-01 23:28:25 -0500

    Add offer repository

---
+++ AppEmpleo/Class/OfferRepository.cs
@@ -0,0 +1,32 @@
+﻿using AppEmpleo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppEmpleo.Class
+{
+    public class OfferRepository
+    {
+        private readonly AppEmpleoContext _appEmpleoContext;
+
+        public OfferRepository(AppEmpleoContext appEmpleoContext)
+        {
+            _appEmpleoContext = appEmpleoContext;
+        }
+
+        public async Task<List<Oferta>> GetListAsync(List<Oferta> listaOfertas)
+        {
+            try
+            {
+                listaOfertas = await _appEmpleoContext.Ofertas.ToListAsync();
+                listaOfertas.OrderByDescending(u => u.FechaInicio);
+
+                return listaOfertas;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            throw new ArgumentException("Error al obtener la lista de ofertas");
+        }
+    }
+}
