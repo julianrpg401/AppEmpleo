9bb08f7dc118a7bf7ebfa81ae17619d49218c041
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-05 21:26:47 -0500

    Rename dinamic class

--- AppEmpleo/wwwroot/js/styles.js
+++ AppEmpleo/wwwroot/js/styles.js
@@ -1,11 +1,11 @@
 ﻿document.querySelectorAll("input").forEach(input => {
     // Agrega la clase "activo" cuando el input recibe el foco
     input.addEventListener("focus", () => {
-        input.classList.add("register__input--active");
+        input.classList.add("form__input--active");
     });
 
     // Quita la clase "activo" cuando el input pierde el foco
     input.addEventListener("blur", () => {
-        input.classList.remove("register__input--active");
+        input.classList.remove("form__input--active");
     });
 });
