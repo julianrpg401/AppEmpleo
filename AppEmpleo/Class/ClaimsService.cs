ee32c245f407b46b20f36db847194ca2c8e0f036
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-09 18:40:47 -0500

    Add claims service to authenticate users

---
+++ AppEmpleo/Class/ClaimsService.cs
@@ -0,0 +1,54 @@
+﻿using AppEmpleo.Models;
+using System.Security.Claims;
+
+namespace AppEmpleo.Class
+{
+    public class ClaimsService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ClaimsService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public List<Claim> CreateClaims(Usuario user)
+        {
+            try
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim("UserId", user.UsuarioId.ToString()),
+                    new Claim(ClaimTypes.Name, user.Nombre),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, user.Rol)
+                };
+
+                return claims;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            throw new ArgumentException();
+        }
+
+        public bool AuthenticatedUser()
+        {
+            return _contextAccessor.HttpContext?.User.Identity?.IsAuthenticated ?? false;
+        }
+
+        public string GetClaim(string claimType)
+            => _contextAccessor.HttpContext?.User.FindFirst(claimType)?.Value ?? string.Empty;
+
+        public string ObtenerNombre()
+            => GetClaim(ClaimTypes.Name);
+
+        public string ObtenerEmail()
+            => GetClaim(ClaimTypes.Email);
+
+        public string ObtenerRol()
+            => GetClaim(ClaimTypes.Role);
+    }
+}
