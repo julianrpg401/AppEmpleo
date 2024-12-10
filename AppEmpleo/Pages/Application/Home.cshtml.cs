d58a0ea9cf7943726565a5d38a794b9ed2e26156
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-09 19:11:13 -0500

    Add claims service

--- AppEmpleo/Pages/Application/Home.cshtml.cs
+++ AppEmpleo/Pages/Application/Home.cshtml.cs
@@ -4,12 +4,19 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Claims;
+using System.ComponentModel;
 
 namespace AppEmpleo.Pages.LandingPage
 {
     public class HomeModel : PageModel
     {
         private readonly OfferRepository _offerRepository;
+        private readonly ClaimsService _claimsService;
+
+        public Usuario Usuario { get; set; }
 
         [BindProperty]
         public Oferta Oferta { get; set; }
@@ -17,14 +24,32 @@ namespace AppEmpleo.Pages.LandingPage
         [BindProperty]
         public List<Oferta> Ofertas { get; set; } = new List<Oferta>();
 
-        public HomeModel(OfferRepository offerRepository)
+        public HomeModel(OfferRepository offerRepository, ClaimsService claimsService)
         {
             _offerRepository = offerRepository;
+            _claimsService = claimsService;
         }
 
         public async void OnGetAsync()
         {
+            GetAuthenticatedUser();
+
             Ofertas = await _offerRepository.GetListAsync(Ofertas);
+        }
+
+        private void GetAuthenticatedUser()
+        {
+            if (!_claimsService.AuthenticatedUser())
+            {
+                RedirectToPage("/Login");
+            }
+
+            Usuario = new Usuario()
+            {
+                Nombre = _claimsService.ObtenerNombre(),
+                Email = _claimsService.ObtenerEmail(),
+                Rol = _claimsService.ObtenerRol(),
+            };
         }
 
         public async Task<IActionResult> OnPostAsync()
