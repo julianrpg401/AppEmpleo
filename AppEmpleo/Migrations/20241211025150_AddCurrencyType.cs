328ab2d5ecee23e07b2f115e20b0404d6f3b7133
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-10 21:53:34 -0500

    Add migration (add currency type)

---
+++ AppEmpleo/Migrations/20241211025150_AddCurrencyType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppEmpleo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrencyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salario",
+                table: "Ofertas",
+                type: "decimal(10,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Moneda",
+                table: "Ofertas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Moneda",
+                table: "Ofertas");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Salario",
+                table: "Ofertas",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldNullable: true);
+        }
+    }
+}
