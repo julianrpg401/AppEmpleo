a91470cb0489325c16c38f5b902a769dbf2507fe
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-13 01:27:19 -0500

    Add currency assistant

---
+++ AppEmpleo/Class/Utilities/CurrencyHelper.cs
@@ -0,0 +1,27 @@
+﻿using AppEmpleo.Models;
+
+namespace AppEmpleo.Class.Utilities
+{
+    public static class CurrencyHelper
+    {
+        public static readonly Dictionary<CurrencyType, string> CurrencyName = new()
+        {
+            { CurrencyType.USD, "Dólar Estadounidense" },
+            { CurrencyType.MXN, "Peso Mexicano" },
+            { CurrencyType.COP, "Peso Colombiano" },
+            { CurrencyType.ARS, "Peso Argentino" },
+            { CurrencyType.BRL, "Real Brasileño" },
+            { CurrencyType.PEN, "Sol Peruano" },
+            { CurrencyType.CLP, "Peso Chileno" },
+            { CurrencyType.UYU, "Peso Uruguayo" },
+            { CurrencyType.DOP, "Peso Dominicano" },
+            { CurrencyType.VES, "Bolívar Venezolano" },
+            { CurrencyType.PYG, "Guaraní Paraguayo" },
+            { CurrencyType.BOB, "Boliviano" },
+            { CurrencyType.CRC, "Colón Costarricense" },
+            { CurrencyType.GTQ, "Quetzal Guatemalteco" },
+            { CurrencyType.HNL, "Lempira Hondureño" },
+            { CurrencyType.PAB, "Balboa Panameño" }
+        };
+    }
+}
