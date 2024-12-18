bf2a870a9ae5cdff727a53daea6e72d7c265821b
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-17 19:22:39 -0500

    Add country helper

---
+++ AppEmpleo/Class/Utilities/CountryHelper.cs
@@ -0,0 +1,30 @@
+﻿using AppEmpleo.Models;
+
+namespace AppEmpleo.Class.Utilities
+{
+    public static class CountryHelper
+    {
+        public static readonly Dictionary<Country, string> CountryName = new()
+        {
+            { Country.MEX, "México" },
+            { Country.COL, "Colombia" },
+            { Country.ARG, "Argentina" },
+            { Country.BRA, "Brasil" },
+            { Country.PER, "Perú" },
+            { Country.CHL, "Chile" },
+            { Country.URY, "Uruguay" },
+            { Country.DOM, "República Dominicana" },
+            { Country.VEN, "Venezuela" },
+            { Country.PRY, "Paraguay" },
+            { Country.BOL, "Bolivia" },
+            { Country.CRI, "Costa Rica" },
+            { Country.GTM, "Guatemala" },
+            { Country.HND, "Honduras" },
+            { Country.PAN, "Panamá" },
+            { Country.CUB, "Cuba" },
+            { Country.ECU, "Ecuador" },
+            { Country.NIC, "Nicaragua" },
+            { Country.SLV, "El Salvador" }
+        };
+    }
+}
