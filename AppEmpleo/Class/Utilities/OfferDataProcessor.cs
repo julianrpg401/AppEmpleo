a042624b19c14bd98a96dcf1280ba022bfba4f1b
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-22 01:09:08 -0500

    New method: OfferFormat

--- AppEmpleo/Class/Utilities/OfferDataProcessor.cs
+++ AppEmpleo/Class/Utilities/OfferDataProcessor.cs
@@ -1,6 +1,25 @@
-﻿namespace AppEmpleo.Class.Utilities
+﻿using AppEmpleo.Models;
+
+namespace AppEmpleo.Class.Utilities
 {
     public class OfferDataProcessor
     {
+        public static Oferta OfferFormat(Oferta offer, Usuario recruiter)
+        {
+            Oferta offerFormatted = new Oferta()
+            {
+                ReclutadorId = recruiter.Reclutador.ReclutadorId,
+                NombreOferta = offer.NombreOferta.ToUpper(),
+                FechaInicio = offer.FechaInicio,
+                FechaCierre = offer.FechaCierre,
+                Pais = offer.Pais.ToUpper(),
+                Moneda = offer.Moneda.ToUpper(),
+                Salario = offer.Salario,
+                Descripcion = offer.Descripcion.ToUpper(),
+                ModalidadTrabajo = offer.ModalidadTrabajo.ToUpper()
+            };
+
+            return offerFormatted;
+        }
     }
 }
