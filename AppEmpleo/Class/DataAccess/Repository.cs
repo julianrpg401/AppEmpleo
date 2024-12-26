44566a7f44d19c918f7996c1e479e57061b6e75f
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-12-25 23:24:39 -0500

    Add base repository for generic methods

---
+++ AppEmpleo/Class/DataAccess/Repository.cs
@@ -0,0 +1,31 @@
+﻿using AppEmpleo.Interfaces;
+using AppEmpleo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppEmpleo.Class.DataAccess
+{
+    public class Repository<T> : IAddAsync<T> where T : class
+    {
+        protected readonly AppEmpleoContext _appEmpleoContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppEmpleoContext appEmpleoContext)
+        {
+            _appEmpleoContext = appEmpleoContext;
+            _dbSet = appEmpleoContext.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            try
+            {
+                await _dbSet.AddAsync(entity);
+                await _appEmpleoContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\nError al insertar en la BD");
+            }
+        }
+    }
+}
