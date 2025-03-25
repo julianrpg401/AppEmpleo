e2a6f0b2eedee41310287b882c8c2b4c017a6bcb
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-25 14:02:42 -0500

    Optimize JS

--- AppEmpleo/wwwroot/js/styles.js
+++ AppEmpleo/wwwroot/js/styles.js
@@ -1,20 +1,21 @@
 ﻿document.querySelectorAll("input").forEach(input => {
-    // Agrega la clase "activo" cuando el input recibe el foco
-    input.addEventListener("focus", () => {
-        input.classList.add("form__input--active");
-    });
+  // Agrega la clase "activo" cuando el input recibe el foco
+  input.addEventListener("focus", () => {
+    input.classList.add("form__input--active");
+  });
 
-    // Quita la clase "activo" cuando el input pierde el foco
-    input.addEventListener("blur", () => {
-        input.classList.remove("form__input--active");
-    });
+  // Quita la clase "activo" cuando el input pierde el foco
+  input.addEventListener("blur", () => {
+    input.classList.remove("form__input--active");
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+  const formOffer = document.getElementById("form");
+  if (formOffer) {
     const plusIcon = document.querySelector(".form__plus--icon");
-    const formOffer = document.getElementById("form");
-
     plusIcon.addEventListener("click", function () {
-        formOffer.classList.toggle("active");
+      formOffer.classList.toggle("active");
     });
+  }
 });
