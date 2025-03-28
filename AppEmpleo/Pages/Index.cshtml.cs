6349a03252e9f227789e102690613e2d2ef818c1
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-28 15:51:04 -0500

    Inject claims service to index model

--- AppEmpleo/Pages/Index.cshtml.cs
+++ AppEmpleo/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using AppEmpleo.Class.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,10 +7,17 @@ namespace AppEmpleo.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ClaimsService? _claimsService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ClaimsService claimsService)
         {
             _logger = logger;
+            _claimsService = claimsService;
+
+            if (_claimsService.AuthenticatedUser())
+            {
+                _claimsService?.UserLogout();
+            }
         }
 
         public void OnGet()
